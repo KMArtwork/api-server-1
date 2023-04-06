7ed64c498bd743c0965d34a83c03fb2b16dba843
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-04-06 13:06:12 -0700

    add review comments

--- src/error-handlers/404.js
+++ src/error-handlers/404.js
@@ -1,6 +1,8 @@
 
 'use strict';
 
+// if you include `error` as the first parameter of the function then express will know to treat this as an error-handler function
+// e.g. function error404 (error, require, response, next) {//code goes here}
 function error404(require, response, next) {
     response.status(404).send('Error 404');
     next();
--- src/error-handlers/500.js
+++ src/error-handlers/500.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// if you include `error` as the first parameter of the function then express will know to treat this as an error-handler function
+// e.g. function error500 (error, require, response, next) {//code goes here}
 function error500(require, response, next) {
   response.status(500).send('Error 500');
   next();
